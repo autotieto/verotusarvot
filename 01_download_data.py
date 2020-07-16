a247e1974216e865de7e51fb95ad57fe65a59cf3
Mikael Gueck <gumi@iki.fi>
2020-07-16 23:28:27 +0300

    md5

--- 01_download_data.py
+++ 01_download_data.py
@@ -4,6 +4,8 @@ import requests
 import argparse
 from bs4 import BeautifulSoup as soup
 import re
+from urllib.parse import urljoin
+import hashlib
 
 RE_YEAR = re.compile('([0-9]{4})', re.M)
 
@@ -23,15 +25,24 @@ def foobar(asd):
 def main(args):
     s = requests.Session()
     r = s.get(args.url)
-
     page = soup(r.text, 'lxml')
-    #with open('sivu.html', 'r') as f:
-    #    page = soup(f, 'lxml')
-
     year_links = page.select('h2:has(> a[id]):contains(Henkil) + ul > li:contains(xls) > a')
+    md5sums = []
     for a in year_links:
-        print(f'''{a['href']}, {RE_YEAR.findall(a.text)[0]}''')
-
+        year = RE_YEAR.findall(a.text)[0]
+        filename = f'data/car_taxes_{year}.xls'
+        url = urljoin(args.url, a['href'])
+        digester = hashlib.md5()
+        with s.get(url, stream=True) as r, open(filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=10240):
+                f.write(chunk)
+                digester.update(chunk)
+        md5 = digester.hexdigest()
+        md5sums.append((md5, filename))
+        print(f'''{year}, {filename}, {md5}, {url}''')
+    with open('data/MD5SUMS', 'w') as f:
+        for md5, filename in md5sums:
+            f.write(f"{md5} {filename}\n")
 
 
 if __name__=='__main__':
