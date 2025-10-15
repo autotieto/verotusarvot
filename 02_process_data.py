d5831f37004270bd999421d4c59b9611ea02eb1f
Mikael Gueck <gumi@iki.fi>
2025-10-15 21:53:37 +0300

    move to pyexcel

--- 02_process_data.py
+++ 02_process_data.py
@@ -1,9 +1,8 @@
 import os
 import glob
 import re
-import xlrd
+import pyexcel
 import polars as pl
-from functools import partial
 
 
 RE_YEAR = re.compile('([0-9]{4})', re.M)
@@ -19,60 +18,50 @@ DTYPES = {
     'tax_eur': pl.Float32,
 }
 
-DEFAULT_STR, DEFAULT_INT, DEFAULT_FLOAT = '', 0, -0.0
+DEFAULT_STR, DEFAULT_INT, DEFAULT_FLOAT, DEFAULT_DATE = '', 0, -0.0, '9999-12-31'
 
-def conv(p, n, t, d):
-    v = p(n).value
-    if v is None or v == '' or v == ' ':
-        return d
+
+def convert(value, transformer, default):
+    if value is None or value == '' or value == ' ':
+        return default
     else:
-        return t(v)
+        return transformer(value)
 
 
-def conv2(cell, t, d):
-    v = cell.value
-    if v is None or v == '' or v == ' ':
-        return d
-    else:
-        return t(v)
-
-
-def conv_new_specification(row):
-    return ''
-
-
-def transfer_normal_sheet(sh, table):
-    for rn in range(2, sh.nrows):
-        p = partial(sh.cell, rn)
-        row = [
-            conv(p, 0, str, DEFAULT_STR),
-            conv(p, 1, str, DEFAULT_STR),
-            conv(p, 2, str, DEFAULT_STR),
-            conv(p, 3, str, DEFAULT_STR),
-            conv(p, 4, int, DEFAULT_INT),
-            conv(p, 5, int, DEFAULT_INT),
-            conv(p, 6, int, DEFAULT_INT),
-            conv(p, 7, float, DEFAULT_FLOAT),
-            conv(p, 8, float, DEFAULT_FLOAT),
-        ]
-        table.append(row)
-
-
-def transfer_2021_feb_sheet(sh, table):
-    for rn in range(2, sh.nrows):
-        row = sh.row(rn)
-        if row[0].ctype == xlrd.XL_CELL_EMPTY:
+def transfer_normal_sheet(sheet: pyexcel.Sheet, table: list[list]):
+    n = 2 if sheet[1, 0] == 'Märke' else 1
+    for row in list(sheet)[n:]:
+        if set(row) == {""} or row[0]== '':
             continue
         item = [
-            conv2(row[0], str, DEFAULT_STR),
-            conv2(row[1], str, DEFAULT_STR),
-            conv_new_specification(row),
+            convert(row[0], str, DEFAULT_STR),
+            convert(row[1], str, DEFAULT_STR),
+            convert(row[2], str, DEFAULT_STR),
+            convert(row[3], str, DEFAULT_STR),
+            convert(row[4], str, DEFAULT_DATE),
+            convert(row[5], str, DEFAULT_DATE),
+            convert(row[6], int, DEFAULT_INT),
+            convert(row[7], float, DEFAULT_FLOAT),
+            convert(row[8], float, DEFAULT_FLOAT),
+        ]
+        table.append(item)
+
+
+def transfer_2021_feb_sheet(sheet: pyexcel.Sheet, table: list[list]):
+    n = 2 if sheet[1, 0] == 'Märke' else 1
+    for row in list(sheet)[n:]:
+        if set(row) == {""} or row[0]== '':
+            continue
+        item = [
+            convert(row[0], str, DEFAULT_STR),
+            convert(row[1], str, DEFAULT_STR),
             '',
-            conv2(row[10], str, DEFAULT_STR),
-            conv2(row[11], str, DEFAULT_STR),
-            conv2(row[12], int, DEFAULT_INT),
-            conv2(row[13], float, DEFAULT_FLOAT),
-            conv2(row[15], float, DEFAULT_FLOAT),
+            '',
+            convert(row[10], str, DEFAULT_DATE),
+            convert(row[11], str, DEFAULT_DATE),
+            convert(row[12], int, DEFAULT_INT),
+            convert(row[13], float, DEFAULT_FLOAT),
+            convert(row[15], float, DEFAULT_FLOAT),
         ]
         table.append(item)
 
@@ -80,15 +69,15 @@ def transfer_2021_feb_sheet(sh, table):
 def main():
     table = []
     for fn in sorted(glob.glob('data/car_taxes*.xls'), key=os.path.basename):
-        print(fn)
-        book = xlrd.open_workbook(fn)
-        for sh in book.sheets():
-            if (sh.ncols == 0 and sh.nrows == 0) or sh.cell(0, 1).value != 'Malli':
+        print(f'procesing {fn}')
+        book = pyexcel.get_book(file_name = fn)
+        for sheet in list(book)[1:]:
+            if len(sheet) == 0 or sheet[0, 1] != 'Malli':
                 continue
-            elif sh.cell(0, 2).value == 'Cm3':
-                transfer_2021_feb_sheet(sh, table)
+            elif sheet[0, 2] == 'Cm3':
+                transfer_2021_feb_sheet(sheet, table)
             else:
-                transfer_normal_sheet(sh, table)
+                transfer_normal_sheet(sheet, table)
 
     df = pl.DataFrame(data=table, schema=DTYPES, orient='row')
     df = df.with_columns(
@@ -98,7 +87,7 @@ def main():
 
     print(df)
 
-    df.write_parquet('./site/verotusarvot.parquet', compression='zstd')
+    df.write_parquet('./site/verotusarvot.parquet', compression_level=19)
 
 if __name__=='__main__':
     main()
